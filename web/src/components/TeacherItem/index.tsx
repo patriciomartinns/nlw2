ba74b605970a7c2a6136eef8e57009ebc2970e02
Patrício Martins <eupatriciomartins@gmail.com>
2020-08-09 01:01:42 -0300

    finish frontend

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+import api from '../../services/api'
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+import './styles.css'
+
+export interface ITeacher {
+  id: number
+  avatar: string
+  bio: string
+  cost: number
+  name: string
+  subject: string
+  whatsapp: string
+}
+
+interface ITeacherItem {
+  teacher: ITeacher
+}
+
+const TeacherItem: React.FC<ITeacherItem> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
+  return (
+      <article className="teacher-item">
+      <header>
+        <img src={teacher.avatar} alt={teacher.name}/>
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
+
+      <p>{teacher.bio}</p>
+
+      <footer>
+        <p>
+          Preço/Hora
+          <strong>R$ {teacher.cost}</strong>
+        </p>
+
+        <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} target="_blank">
+          <img src={whatsappIcon} alt="Whatsapp"/>
+          Entrar em contato
+        </a>
+      </footer>
+    </article>
+  )
+}
+
+export default TeacherItem
